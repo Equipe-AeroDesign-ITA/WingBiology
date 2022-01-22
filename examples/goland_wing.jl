4975816d0c7a6b658f1d23f7e749ddb14cac22cc
pedrosecchi67 <pedrosecchimail@gmail.com>
2022-01-21 21:10:51 -0300

    Added Goland wing example for flutter prediction. Reasonable results seem to be achieved, but they show a rather intense dependency on strucural damping, which ought to have its coefficient defined for better cutoff frequencies

---
+++ examples/goland_wing.jl
@@ -0,0 +1,76 @@
+using WingBiology
+
+c = 1.8288
+b = 2 * 6.096
+
+e = 0.33 - 0.25
+xCG = 0.43 - 0.25
+
+m = 35.71
+Ixx = 8.64
+
+GJ = 0.99e6
+EIxx = 9.77e6
+
+U∞ = 165.0
+ρ = 1.020
+α = 0.05
+
+g = 8e-3
+
+acft = Aircraft()
+
+ls = WingSectInfo(
+    [0.0, - b / 2, 0.0],
+    c;
+    e = e,
+    xCG = xCG,
+    m = m,
+    Ixx = Ixx,
+    EIy = EIxx,
+    GJ = GJ,
+    g = g
+)
+cs = WingSectInfo(
+    [0.0, 0.0, 0.0],
+    c;
+    e = e,
+    xCG = xCG,
+    m = m,
+    Ixx = Ixx,
+    EIy = EIxx,
+    GJ = GJ,
+    g = g
+)
+rs = WingSectInfo(
+    [0.0, b / 2, 0.0],
+    c;
+    e = e,
+    xCG = xCG,
+    m = m,
+    Ixx = Ixx,
+    EIy = EIxx,
+    GJ = GJ,
+    g = g
+)
+
+lwing_pts, lwing_sects = interpolate!(acft, 30, ls, cs)
+rwing_pts, rwing_sects = interpolate!(acft, 30, cs, rs)
+
+fixed_points = [
+    rwing_pts[1]
+]
+
+q = get_state(acft, U∞; α = α)
+
+Arnoldi_solve!(acft, q, U∞; α = α, fixed_points = fixed_points, ρ = ρ)
+
+plot_aircraft(acft, q)
+
+A, info = assumed_modes_solve(acft, q, U∞; ρ = ρ, α = α, fixed_points = fixed_points)
+
+λs = info.λ
+
+for (i, λ) in enumerate(λs)
+    @show i, λ
+end
