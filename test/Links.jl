47df2e1959d1da4de4eb8bc690db320e1b7783fd
pedrosecchi67 <pedrosecchimail@gmail.com>
2022-02-06 00:14:31 -0300

    Ran tests and figured that the singularities were due to zero angular accel. being caused to points when their mass is identically zero

--- test/Links.jl
+++ test/Links.jl
@@ -13,22 +13,46 @@
         EA = 10.0
     )
 
-    add_link!(acft, i1)
-    add_link!(acft, i2, i3)
-    add_link!(acft, i2, i4)
-
     add_mass!(acft, i3, 1.0)
     add_mass!(acft, i4, 1.0)
 
-    K = get_elastic_matrix(acft)
+    add_link!(acft, i1)
 
     u = get_state(acft, 0.0)
-
     set_u(u, i2, 1.5)
-
     qd, _ = state_space(acft, u, 0.0)
 
-    @show get_̇u(qd)
+    acc_free = get_̇u(qd)
+
+    add_link!(acft, i2, i3)
+    add_link!(acft, i2, i4)
+
+    u = get_state(acft, 0.0)
+    set_u(u, i2, 1.5)
+    qd, _ = state_space(acft, u, 0.0)
+
+    acc_const = get_̇u(qd)
+
+    ms = [
+        m.m for m in acft.masses
+    ]
+
+    @assert ms ⋅ acc_const ≈ ms ⋅ acc_free
+
+    K = get_elastic_matrix(acft)
+    K = Array(K)
+
+    Ndof2 = ndofs(acft) ÷ 2
+    K = (K[(Ndof2 + 1):end, 1:Ndof2])
+
+    eig = eigen(K)
+
+    zero_eigs = abs.(eig.values) .< 1e-5
+
+    zero_vals = eig.values[zero_eigs]
+    zero_vecs = eig.vectors[:, zero_eigs]
+
+    @show zero_vecs
 
     true
 end
